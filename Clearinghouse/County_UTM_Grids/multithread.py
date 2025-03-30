c37d0854fcdd7a9372ae166794cbf0729b196f6f
Nick Rupert <nickrupert7@gmail.com>
2025-03-29 21:05:57 -0400

    chore: Update number of threads for Tank

--- Clearinghouse/County_UTM_Grids/multithread.py
+++ Clearinghouse/County_UTM_Grids/multithread.py
@@ -2,12 +2,14 @@
 from multiprocessing.dummy import Pool as ThreadPool
 import subprocess
 
+NUM_THREADS = 96 * 3
+
 def process(i):
   print(f'Processing {str(i)}')
   subprocess.run(["node", "intersect.js", str(i)])
 
 def main():
-    pool = ThreadPool(16)
+    pool = ThreadPool(NUM_THREADS)
     pool.map(process, range(3143))
     pool.close()
     pool.join()
