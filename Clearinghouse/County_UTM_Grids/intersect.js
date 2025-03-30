7a6764ae1deaeb1c370bf0b971eeca2f7a194423
Nick Rupert <nickrupert7@gmail.com>
2025-03-29 21:29:14 -0400

    perf: Increase memory efficiency

--- Clearinghouse/County_UTM_Grids/intersect.js
+++ Clearinghouse/County_UTM_Grids/intersect.js
@@ -4,10 +4,6 @@ const utmZones = require('../UTM_Grids/UTM_Zones.json')
 const fs = require('fs')
 const utmZoneTiles = []
 
-for (let i = 1; i <= 19; i++) {
-  utmZoneTiles[i] = require(`../UTM_Grids/UTM_Grids/${i}.json`)
-}
-
 const main = async () => {
   const i = process.argv[2]
 
@@ -18,9 +14,10 @@ const main = async () => {
 
     for (let j in utmZones.features) {
       const utmZone = utmZones.features[j]
-      console.log(turf.feature(utmZone), turf.feature(county).geometry)
 
       if (turf.booleanIntersects(turf.feature(utmZone).geometry, turf.feature(county).geometry)) {
+	utmZoneTiles[utmZone.properties.UTM_ZONE] = require(`../UTM_Grids/UTM_Grids/${utmZone.properties.UTM_ZONE}.json`)
+
         for (let k in utmZoneTiles[utmZone.properties.UTM_ZONE].features) {
           const tile = utmZoneTiles[utmZone.properties.UTM_ZONE].features[k]
           console.log(`(${i}/${counties.features.length})(${j})(${k}/${utmZoneTiles[utmZone.properties.UTM_ZONE].features.length}) Checking ${county.properties.NAME} tile x:${tile.properties.Name_X} y:${tile.properties.Name_Y}`)
