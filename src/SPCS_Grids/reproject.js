244c84eea0ae21b50304c8b0225aa54f601ad571
Nick Rupert <nickrupert7@gmail.com>
2025-04-04 15:59:30 -0400

    chore: Add shapefiles for SPCS_Grides, rearrange shapefiles for state spcs utm counties

--- src/SPCS_Grids/reproject.js
+++ src/SPCS_Grids/reproject.js
@@ -1,18 +1,25 @@
-const spcsZones = require('./SPCS_Zones.json')
+const spcsZones = require('../../data/SPCS_Zones.json')
 const { execSync } = require('child_process');
+const fs = require('fs')
 
 const main = async () => {
   for (let zone of spcsZones.features) {
-    const state = zone.properties.STATE.replaceAll(' ', '\\ ')
+    const state = zone.properties.STATE.replaceAll(' ', '_')
     const zoneName = zone.properties.SP_ZONE.replaceAll(' ', '_')
     const projection = `${zone.properties.SPCS_AUTH}:${zone.properties.SPCS_ID}`
 
-    const inputPath = `./SPCS_Grids_RAW/${state}/${zoneName}_Grid.geojson`
-    const outputPath = `./SPCS_Grids_RAW/${state}/${zoneName}.shapefile`
+    const inputPath = `../../data/SPCS_Grids/${state}/${zoneName}_Grid.geojson`
+    const outputPath = `../../data/SPCS_Grids/${state}/${zoneName}.shapefile`
     
     console.log(`Reprojecting ${zoneName}`)
     const command = `ogr2ogr -f "ESRI Shapefile" -skipfailures ${outputPath} ${inputPath} -t_srs ${projection}`
     execSync(command)
+    
+    fs.renameSync(`${outputPath}/${zoneName}_Grid.shp`, `../../data/SPCS_Grids/${state}/${zoneName}_Grid.shp`)
+    fs.renameSync(`${outputPath}/${zoneName}_Grid.prj`, `../../data/SPCS_Grids/${state}/${zoneName}_Grid.prj`)
+    fs.renameSync(`${outputPath}/${zoneName}_Grid.shx`, `../../data/SPCS_Grids/${state}/${zoneName}_Grid.shx`)
+    fs.renameSync(`${outputPath}/${zoneName}_Grid.dbf`, `../../data/SPCS_Grids/${state}/${zoneName}_Grid.dbf`)
+    fs.rmdirSync(outputPath)
   }
 }
 
