f8769ab6a75a04544fe81f79cfd6be802c6021e7
Nick Rupert <nickrupert7@gmail.com>
2025-04-04 14:38:21 -0400

    fix: Fix sort script

--- src/Sort_DEMs/sort.js
+++ src/Sort_DEMs/sort.js
@@ -16,19 +16,19 @@ const main = async () => {
         const countyTiles = require(`#data/County_UTM_Grids/${county.properties.STATE}/${county.properties.NAME}.json`)
 
         for (let tile of countyTiles.features) {
-            const matches = demTiles.filter(t => t.x === parseInt(tile.Name_X) && t.y === parseInt(tile.Name_Y))
+            const matches = demTiles.filter(t => t.x === parseInt(tile.properties.Name_X) && t.y === parseInt(tile.properties.Name_Y))
 
             if (matches.length === 0) {
-                const error = `[${county.properties.NAME}] X: ${tile.Name_X}, Y: ${tile.Name_Y} FOUND 0 MATCHES`
+                const error = `[${county.properties.NAME}] X: ${tile.properties.Name_X}, Y: ${tile.properties.Name_Y} FOUND 0 MATCHES`
                 console.log(error)
                 errors.push(error)
             } else if (matches.length > 1) {
-                const error = `[${county.properties.NAME}] X: ${tile.Name_X}, Y: ${tile.Name_Y} FOUND ${matches.length} MATCHES`
+                const error = `[${county.properties.NAME}] X: ${tile.properties.Name_X}, Y: ${tile.properties.Name_Y} FOUND ${matches.length} MATCHES`
                 console.log(error)
                 errors.push(error)
             }
 
-            const destFolder = `/mnt/z/${STATE}/${county.properties.NAME}/Tif_Files_UTM`
+            const destFolder = `/mnt/z/${STATE_FOLDER}/${county.properties.NAME.replaceAll(' ', '_')}_County_Contours/Tif_Files_UTM`
 
             if (!fs.existsSync(destFolder)) {
                 fs.mkdirSync(destFolder, { recursive: true })
@@ -37,11 +37,11 @@ const main = async () => {
             for (let match of matches) {
                 const fileNameParts = match.url.split('/')
                 const fileName = fileNameParts[fileNameParts.length - 1]
-                console.log(`Copying /mnt/z/${STATE}/Tif_Files_UTM/${match.project}/${fileName} -> ${destFolder}/${fileName}`)
-                fs.copyFileSync(`/mnt/z/${STATE}/Tif_Files_UTM/${match.project}/${fileName}`, `${destFolder}/${fileName}`)
+                console.log(`Copying /mnt/z/${STATE_FOLDER}/Tif_Files_UTM/${match.project}/${fileName} -> ${destFolder}/${fileName}`)
+                fs.copyFileSync(`/mnt/z/${STATE_FOLDER}/Tif_Files_UTM/${match.project}/${fileName}`, `${destFolder}/${fileName}`)
             }
 
-            fs.writeFileSync(`/mnt/z/${STATE}/DEM_COPY.log`, JSON.stringify(errors, null, 2))
+            fs.writeFileSync(`/mnt/z/${STATE_FOLDER}/DEM_COPY.log`, JSON.stringify(errors, null, 2))
         }
     }
 }
