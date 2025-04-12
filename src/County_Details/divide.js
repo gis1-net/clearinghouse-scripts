1ab9ca79f4b611f2140688fd8a77c9c00a73394d
Nick Rupert <nickrupert7@gmail.com>
2025-04-12 11:16:10 -0400

    chore: Update State Counties Detailed with coordinate system info

--- src/County_Details/divide.js
+++ src/County_Details/divide.js
@@ -1,6 +1,7 @@
 const counties = require('#data/US_Counties_Detailed.json')
 const { groupBy } = require('lodash')
 const fs = require('fs')
+const { execSync } = require('child_process');
 
 const main = async () => {
     const stateGroups = groupBy(counties.features, c => c.properties.STATE)
@@ -8,8 +9,19 @@ const main = async () => {
     for (let state in stateGroups) {
         const counties = stateGroups[state]
 
-        fs.writeFileSync(`../../data/State_Counties_Detailed/${state}.json`, JSON.stringify({ type: 'FeatureCollection', featuers: counties }, null, 2))
+        const inputPath = `../../data/State_Counties_Detailed/${state.replaceAll(' ', '_')}.json`
+        const outputPath = `../../data/State_Counties_Detailed/${state.replaceAll(' ', '_')}.shapefile`
+
+        fs.writeFileSync(inputPath, JSON.stringify({ type: 'FeatureCollection', features: counties }, null, 2))
         
+        const command = `ogr2ogr -f "ESRI Shapefile" -skipfailures ${outputPath} ${inputPath}`
+        execSync(command)
+        
+        fs.renameSync(`${outputPath}/${state.replaceAll(' ', '_')}.shp`, `../../data/State_Counties_Detailed/${state.replaceAll(' ', '_')}.shp`)
+        fs.renameSync(`${outputPath}/${state.replaceAll(' ', '_')}.prj`, `../../data/State_Counties_Detailed/${state.replaceAll(' ', '_')}.prj`)
+        fs.renameSync(`${outputPath}/${state.replaceAll(' ', '_')}.shx`, `../../data/State_Counties_Detailed/${state.replaceAll(' ', '_')}.shx`)
+        fs.renameSync(`${outputPath}/${state.replaceAll(' ', '_')}.dbf`, `../../data/State_Counties_Detailed/${state.replaceAll(' ', '_')}.dbf`)
+        fs.rmdirSync(outputPath)
     }
 }
 
